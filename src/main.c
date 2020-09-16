4bbd856ecf2c107c9271ace76212d91ed1567ece
Kyle0122 <root@Awsl.localdomain>
2020-09-16 23:42:50 +0800

    add Folders

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//pi BBP位算法
+int main() {
+	double pi = 0.0;
+	double deno = 1.0;
+	for(int k = 0; deno != 0; k++){
+		pi += (4.0/(8*k +1.0) - 2.0/(8*k +4.0) - 1.0/(8*k +5.0) - 1.0/(8*k +6.0)) * deno;
+		deno = deno / 16.0;
+		printf("pi(%d):\t%.50f\n", k, pi);
+	}
+}
+
